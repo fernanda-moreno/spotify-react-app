4e123b572fc70e7ee454031cbe2642e861dfcccf
Fernanda Moreno <46096003+fernanda-moreno@users.noreply.github.com>
2019-08-11 17:32:19 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
-export const clientId = "50aa59db8cbf4362a6e67f863aa766e2";
+export const clientId = "YOUR_CLIENT_ID";
 export const redirectUri = "http://localhost:3000";
 export const scopes = [
     "user-top-read",
