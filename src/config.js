4aed615a93b167eb466d42e57650de7d3ae1c14c
Fernanda Moreno <46096003+fernanda-moreno@users.noreply.github.com>
2019-08-11 17:31:15 -0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "50aa59db8cbf4362a6e67f863aa766e2";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
